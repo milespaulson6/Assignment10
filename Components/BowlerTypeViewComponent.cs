409ae4ea06d1df6a2098cc4e1a944522b671ae91
Miles Paulson <milespaulson6@gmail.com>
2021-04-05 18:02:05 -0600

    yew

---
+++ Components/BowlerTypeViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Assignment10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Components
+{
+    public class BowlerTypeViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext context;
+        public BowlerTypeViewComponent(BowlingLeagueContext ctx)
+        {
+            context = ctx;
+        }
+        public IViewComponentResult Invoke()
+        {
+
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x));
+               
+        }
+    }
+}
---
+++ Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Assignment10.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-info")]
+    public class PaginationTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlInfo;
+        public PaginationTagHelper(IUrlHelperFactory uhf)
+        {
+            urlInfo = uhf;
+        }
+
+        public PageNumberingInfo PageInfo { get; set; }
+
+        [HtmlAttributeName(DictionaryAttributePrefix ="page-url-")]
+        public Dictionary<string, object> KeyValuePairs { get; set; } = new Dictionary<string, object>();
+        [HtmlAttributeNotBound]
+        [ViewContext]
+
+        public ViewContext ViewContext { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            TagBuilder finishedTag = new TagBuilder("div");
+            
+            for (int i = 1; i <= PageInfo.NumPages; i++)
+            {
+                IUrlHelper urlHelp = urlInfo.GetUrlHelper(ViewContext);
+
+                TagBuilder individualTag = new TagBuilder("a");
+
+                KeyValuePairs["pageNum"] = i;
+                individualTag.Attributes["href"] = urlHelp.Action("Index", KeyValuePairs);
+                individualTag.InnerHtml.Append(i.ToString());
+
+                finishedTag.InnerHtml.AppendHtml(individualTag);
+            }
+            
+
+            output.Content.AppendHtml(finishedTag.InnerHtml);
+            //base.Process(context, output);
+        }
+    }
+}
